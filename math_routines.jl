889263a9f1a521c1f8d9c7d863873df80f356837
John Wise <jwise@physics.gatech.edu>
2021-08-02 16:53:57 -0400

    Adding local cubic monotonic interp routines
    From Hayek+ (2010) Appendix B

--- math_routines.jl
+++ math_routines.jl
@@ -115,7 +115,7 @@ end
 
 function bezier_interp_coef(s0, sm, sp, dtaum, dtaup, linear=false)
     # Kunasz & Auer (1988), Equations (7a) - (9c)
-    # Hayek et al. (2010), Appendix A
+    # Hayek et al. (2010), Appendix A, psim = Psi_d to conform with Kunasz88 notation
     u0(x) = 1 - exp(-x)
     u1(x) = x - u0(x)
     u2(x) = x^2 - 2*u1(x)
@@ -172,7 +172,7 @@ struct field_deriv{T<:Real}
     right::Array{T}
 end
 
-function grid_deriv(f)
+function grid_deriv(f, dx=1)
     nd = ndims(f)
     ns = size(f)
     vec_size = Tuple(Base.Iterators.flatten((ns,nd)))
@@ -183,32 +183,77 @@ function grid_deriv(f)
         fprime.left[:,:,i]  = f - circshift(f,shift)
         fprime.right[:,:,i] = circshift(f,-shift) - f
     end
+    fprime.left /= dx
+    fprime.right /= dx
     return fprime
 end
 
-function interp_zero(f,fp,dx)
+# Interpolation functions in 1D
+# Perform multi-D interpolation in sweeps
+
+function interp_zero(f,fp,x,dx=1)
     # Nearest neighbor
-    return 0
+    idx = floor.(Int, x/dx)
+    return f[idx]
 end
 
-function interp_linear(f,fp,dx)
-    # Linear Interpolation in 1D (sweeps)
-    result = Array{Float}(undef,size(dx)[1])
-    @. result = f[:]
-    #return f0 + (f1-f0) * dx
+function interp_linear(f,fp,x,dx=1)
+    # 1D Linear Interpolation (sweeps)
+    t = mod.(x,dx)
+    idx = floor.(Int, x/dx)
+    result = f[idx] .+ t.*(f[idx+1] .- f[idx-1])/dx
+    return result
 end
 
-function interp_quad(f,fp,dx)
-    # Monotonic quadratic (Hayek+ 2010, Appendix B)
-    return 0
+function fp_harm(fp)
+    # Hayek+ 2010, Equation B.3
+    # Harmonic mean of left- and right-handed differences to remove wiggles and overshoots in strong gradients
+    fpnew = zeros(fp.size)
+    pp = fp.left * fp.right .> 0
+    fpnew[pp] = (fp.left[pp] .* fp.right[pp]) ./ (0.5 * (fp.left[pp] .+ fp.right[pp]))
+    return fpnew
 end
 
-function interp_cubic(f,fp,dx)
-    # Monotonic cubic (Hayek+ 2010, Appendix B)
-    return 0
+function interp_quad(f,fp,x,dx=1)
+    # 1D Monotonic quadratic (Hayek+ 2010, Appendix B, equation B.5)
+    nx = size(x)
+    t = mod.(x,dx)
+    left = t .< 0.5
+    right = t .> 0.5
+    alpha = zeros(nx)
+    beta = zeros(nx)
+    gamma = zeros(nx)
+    delta = zeros(nx)
+    # Interpolation on the left-hand side
+    alpha[left] = 1 .- t[left].^2
+    beta[left] = t[left].^2
+    gamma[left] = t[left] .* (1 .- t[left]) * dx
+    # Interpolation on the right-hand side
+    alpha[right] = (1 .- t[right]).^2
+    beta[right] = t[right] .* (2 .- t[right])
+    delta[right] = t[right] .* (t[right] .- 1) * dx
+    fpnew = fp_harm(fp)
+    result = alpha .* f[idx] + beta .* f[idx+1] + gamma .* fpnew[idx] + delta .* fpnew[idx+1]
+    return result
+end
+
+function interp_cubic(f,fp,x,dx=1)
+    # 1D Monotonic cubic (Hayek+ 2010, Appendix B)
+    nx = size(x)
+    t = mod.(x, dx)
+    idx = floor.(Int, x/dx)
+    alpha = 2*t.^3 .- 3*t.^2 .+ 1
+    beta = 3*t.^2 .- 2*t.^3
+    gamma = (t.^3 .- 2*t.^2 .+ t)*dx
+    delta = (t.^3 .- t.^2)*dx
+    fpnew = fp_harm(fp)
+    result = alpha .* f[idx] + beta .* f[idx+1] + gamma .* fpnew[idx] + delta .* fpnew[idx+1]
+    return result
 end
 
 function interpolate(f, x, order=1)
+    # Interpolate positions x (rank order) from field f (rank order)
+    # Assume that x are in units of cell widths (dx = 1)
     if order == 0
         interp_fn = interp_zero
     elseif order == 1
