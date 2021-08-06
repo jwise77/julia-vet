94552bc660e6d851962332f79853e02c3ea2d381
John Wise <jwise@physics.gatech.edu>
2021-08-06 16:46:10 -0400

    Adding 1D Bezier interp from H20

--- bezier-h20.jl
+++ bezier-h20.jl
@@ -4,7 +4,9 @@
 # We only need the 1D and 2D cases for short characteristics because we
 # need the quantities on the ray intersections with the cell edges 
 # (2D domain) or cell faces (3D domain). No need for linear interpolation
-# routines because of the control parameter omega.
+# routines because of the control parameter omega. In Appendix B of 
+# Hennicker+ (2020), we will only use the [x_i, x_(i+1)] case because we'll 
+# floor the values.
 #
 # Also see bezier-hayek.jl
 #
@@ -20,3 +22,34 @@
 #
 # Output: interpolated quantity
 
+function bezier_interp_1d(f, x, omega=1)
+    idx0 = floor.(Int, x)
+    idxm = idx0 .- 1
+    idxp = idx0 .+ 1
+    xi = Float64.(idx0)
+    # Interpolation point (B.11)
+    t = x .- xi
+    # Control point (B.12)
+    fc = f[idx0] .+ 0.5 * (omega .* (f[idxp] .- f[idx0]) .+ 
+        (1 .- omega) .* (f[idx0] - f[idxm]))
+
+    # Shape parameters to maintain monotonicity (B.17-18)
+    # Note: Not sure if Julia will deal with divide by zeroes nicely
+    omegai = 1 ./ (1 .- (f[idxp] - f[idx0]) / (f[idx0] - f[idxm]))
+    omegap = 1 .+ 1 ./ (1 .- (f[idx0] - f[idxp]) / (f[idxp] - f[idx0]))
+    omega0 = 0.5 * (omegai + omegap)
+    # Restrict omega to be within the range [omegai, omegap].
+    # If outside, set to be the arthimetic mean of omegai and omegap.
+    om = omega .* ones(Float64, size(x))
+    mask = (omega .< omegai) .| (omega .> omegap)
+    om[mask] = omega0[mask]
+
+    # Interpolation coefficients. (B.14-16) setting dx = 1
+    atilde = (om - 1) .* t .- (om - 1) .* t.^2
+    btilde = 1 .- (2*om - 1) .* t .+ 2*(om - 1) .* t.^2
+    ctilde = om .* t + (1 - om) .* t.^2
+
+    # Evaluate the Bezier polynomial with the coefficients
+    result = atilde .* f[idxm] .+ btilde .* f[idx0] .+ ctilde .* f[idx1]
+    return result
+end
