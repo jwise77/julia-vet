5a5a3a8a49a71ca9e5f959107d1afd600dc03465
John Wise <jwise@physics.gatech.edu>
2021-08-12 11:32:18 -0400

    Serializing the routines so they can automatically vectorized
    Now the Bezier routines can be called like function.() instead of
    including dots in every operation. Makes for much cleaner code.

--- bezier-h20.jl
+++ bezier-h20.jl
@@ -22,42 +22,37 @@
 #
 # Output: interpolated quantity
 
-function bezier_interp_1dv(f, x, omega=1, return_coeff=false)
+function bezier_interp_1dv(f::AbstractArray, x::Number, omega=1, return_coeff=false)
     # Version that takes entire array (row or column)
     # coeff_only
     # * true = returns interpolation coefficients only. Useful for 2D.
     # * false = return interpolated value
-    idx0 = floor.(Int, x)
-    idxm = idx0 .- 1
-    idxp = idx0 .+ 1
-    xi = Float64.(idx0)
+    idx0 = floor(Int, x)
+    idxm = idx0 - 1
+    idxp = idx0 + 1
+    xi = Float64(idx0)
     # Interpolation point (B.11)
-    t = x .- xi
+    t = x - xi
     # Control point (B.12)
-    fc = f[idx0] .+ 0.5 * (omega .* (f[idxp] .- f[idx0]) .+ 
-        (1 .- omega) .* (f[idx0] - f[idxm]))
+    fc = f[idx0] + 0.5 * (omega * (f[idxp] - f[idx0]) + 
+        (1 - omega) * (f[idx0] - f[idxm]))
 
     # Shape parameters to maintain monotonicity (B.17-18)
     # Note: Not sure if Julia will deal with divide by zeroes nicely
-    omegai = 1 ./ (1 .- (f[idxp] .- f[idx0]) ./ (f[idx0] .- f[idxm]))
-    omegap = 1 .+ 1 ./ (1 .- (f[idx0] .- f[idxm]) ./ (f[idxp] .- f[idx0]))
-    omega_min = min.(omegai, omegap)
-    omega_max = max.(omegai, omegap)
-    omega0 = 0.5 * (omega_min .+ omega_max)
-    # Restrict omega to be within the range [omegai, omegap].
-    # If outside, set to be the arthimetic mean of omegai and omegap.
-    om = omega .* ones(Float64, size(x))
-    mask = (omega .< omega_min) .| (omega .> omega_max)
-    #om[mask] = omega0[mask]
-    om[mask] = max.(min.(omega_min[mask], 0.5), 1.0)
+    omegai = 1 / (1 - (f[idxp] - f[idx0]) / (f[idx0] - f[idxm]))
+    omegap = 1 + 1 / (1 - (f[idx0] - f[idxm]) / (f[idxp] - f[idx0]))
+    # Restrict omega to range of [omegai,omegap] and [0.5,1]
+    omega_min = max(min(omegai, omegap), 0.5)
+    omega_max = min(max(omegai, omegap), 1.0)
+    om = min(max(omega, omega_min), omega_max)
 
     # Interpolation coefficients. (B.14-16) setting dx = 1
-    atilde = (om .- 1) .* t .- (om .- 1) .* t.^2
-    btilde = 1 .- (2 .* om .- 1) .* t .+ 2 .* (om .- 1) .* t.^2
-    ctilde = om .* t .+ (1 .- om) .* t.^2
+    atilde = (om - 1) * t - (om - 1) * t^2
+    btilde = 1 - (2 * om - 1) * t + 2 * (om - 1) * t^2
+    ctilde = om * t + (1 - om) * t^2
 
     # Evaluate the Bezier polynomial with the coefficients
-    result = atilde .* f[idxm] .+ btilde .* f[idx0] .+ ctilde .* f[idxp]
+    result = atilde * f[idxm] + btilde * f[idx0] + ctilde * f[idxp]
     if return_coeff
         coeff = Dict("a"=>atilde, "b"=>btilde, "c"=>ctilde)
         result = (result, coeff)
@@ -65,29 +60,24 @@ function bezier_interp_1dv(f, x, omega=1, return_coeff=false)
     return result
 end
 
-function bezier_interp_1d(fm, f0, fp, x, omega=1, return_coeff=false)
+function bezier_interp_1d(fm::Number, f0::Number, fp::Number, x::Number, omega=1, return_coeff=false)
     # Version that takes three points (j-1, j, j+1) and an x-coordinate for
     # interpolation instead of an array.
     # Same operations as the full array version.
-    t = mod.(x,1)
-    fc = f0 .+ 0.5 * (omega .* (fp .- f0) .+ (1 .- omega) .* (f0 .- fm))
+    t = mod(x,1)
+    fc = f0 + 0.5 * (omega * (fp - f0) + (1 - omega) * (f0 - fm))
 
-    omegai = 1 ./ (1 .- (fp .- f0) ./ (f0 .- fm))
-    omegap = 1 .+ 1 ./ (1 .- (f0 .- fm) ./ (fp .- f0))
-    omega_min = min.(omegai, omegap)
-    omega_max = max.(omegai, omegap)
-    omega0 = 0.5 * (omegai .+ omegap)
+    omegai = 1 / (1 - (fp - f0) / (f0 - fm))
+    omegap = 1 + 1 / (1 - (f0 - fm) / (fp - f0))
+    omega_min = max(min(omegai, omegap), 0.5)
+    omega_max = min(max(omegai, omegap), 1.0)
+    om = min(max(omega, omega_min), omega_max)
 
-    om = omega .* ones(Float64, size(x))
-    mask = (omega .< omega_min) .| (omega .> omega_max)
-    #om[mask] = omega0[mask]
-    om[mask] = max.(min.(omega_min[mask], 0.5), 1.0)
+    atilde = (om - 1) * t - (om - 1) * t^2
+    btilde = 1 - (2 * om - 1) * t + 2 * (om - 1) * t^2
+    ctilde = om * t + (1 - om) * t^2
 
-    atilde = (om .- 1) .* t .- (om .- 1) .* t.^2
-    btilde = 1 .- (2 .* om .- 1) .* t .+ 2 .* (om .- 1) .* t.^2
-    ctilde = om .* t + (1 .- om) .* t.^2
-
-    result = atilde .* fm .+ btilde .* f0 .+ ctilde .* fp
+    result = atilde * fm + btilde * f0 + ctilde * fp
     if return_coeff
         coeff = Dict("a"=>atilde, "b"=>btilde, "c"=>ctilde)
         result = (result, coeff)
@@ -95,45 +85,45 @@ function bezier_interp_1d(fm, f0, fp, x, omega=1, return_coeff=false)
     return result
 end
 
-function get_indices(f, ix, iy)
+function get_indices(f::AbstractMatrix, ix::AbstractArray, iy::AbstractArray)
     # Does what I expect f[ix,iy] would do
     # Returns an array of size (ix) instead of (ix,iy)
     return [f[ix[i],iy[i]] for i in eachindex(ix,iy)]
 end
 
-function bezier_interp_2d(f, x, y, omega=1)
+function bezier_interp_2d(f::AbstractArray, x::Number, y::Number, omega=1)
     if ndims(f) != 2
         error("Field f must be 2D.")
     end
 
     # Central and adjacent indices
-    idx0 = floor.(Int, x)
-    idxm = idx0 .- 1
-    idxp = idx0 .+ 1
-    idy0 = floor.(Int, y)
-    idym = idy0 .- 1
-    idyp = idy0 .+ 1
+    idx0 = floor(Int, x)
+    idxm = idx0 - 1
+    idxp = idx0 + 1
+    idy0 = floor(Int, y)
+    idym = idy0 - 1
+    idyp = idy0 + 1
 
     # Interpolated values (fy) and coefficients (cy) for j-1, j, and j+1 rows
-    fxm, cxm = bezier_interp_1dv(f[:,idym], x, omega, true)
-    fx0, cx0 = bezier_interp_1dv(f[:,idy0], x, omega, true)
-    fxp, cxp = bezier_interp_1dv(f[:,idyp], x, omega, true)
+    fxm, cxm = bezier_interp_1dv.(Ref(f[:,idym]), x, omega, true)
+    fx0, cx0 = bezier_interp_1dv.(Ref(f[:,idy0]), x, omega, true)
+    fxp, cxp = bezier_interp_1dv.(Ref(f[:,idyp]), x, omega, true)
 
     # Interpolation values (finterp) and coefficients (cx) along the y-axis 
     # at the x-value
-    fy, cy = bezier_interp_1d(fxm, fx0, fxp, y, omega, true)
+    fy, cy = bezier_interp_1d.(fxm, fx0, fxp, y, omega, true)
 
     # 2D interpolation with all coefficients (Equation C.1)
     result = 
-        cy["a"] .* cxm["a"] .* get_indices(f, idxm, idym) + 
-        cy["a"] .* cxm["b"] .* get_indices(f, idx0, idym) +
-        cy["a"] .* cxm["c"] .* get_indices(f, idxp, idym) +
-        cy["b"] .* cx0["a"] .* get_indices(f, idxm, idy0) + 
-        cy["b"] .* cx0["b"] .* get_indices(f, idx0, idy0) +
-        cy["b"] .* cx0["c"] .* get_indices(f, idxp, idy0) +
-        cy["c"] .* cxp["a"] .* get_indices(f, idxm, idyp) + 
-        cy["c"] .* cxp["b"] .* get_indices(f, idx0, idyp) +
-        cy["c"] .* cxp["c"] .* get_indices(f, idxp, idyp)
+        cy["a"] * cxm["a"] * f[idxm, idym] + 
+        cy["a"] * cxm["b"] * f[idx0, idym] +
+        cy["a"] * cxm["c"] * f[idxp, idym] +
+        cy["b"] * cx0["a"] * f[idxm, idy0] + 
+        cy["b"] * cx0["b"] * f[idx0, idy0] +
+        cy["b"] * cx0["c"] * f[idxp, idy0] +
+        cy["c"] * cxp["a"] * f[idxm, idyp] + 
+        cy["c"] * cxp["b"] * f[idx0, idyp] +
+        cy["c"] * cxp["c"] * f[idxp, idyp]
     
     return result
 end
--- test.jl
+++ test.jl
@@ -1,5 +1,6 @@
+using Debugger
 import Distributions: Uniform
-include("general_interpolation.jl")
+#include("general_interpolation.jl")
 include("bezier-h20.jl")
 
 N = 6
@@ -15,19 +16,24 @@ one = rand(rfn, N)
 two = rand(rfn, N,N)
 three = rand(rfn, N,N,N)
 
-fp = grid_deriv(one)
-fp2 = grid_deriv(two)
-fp3 = grid_deriv(three)
+#fp = grid_deriv(one)
+#fp2 = grid_deriv(two)
+#fp3 = grid_deriv(three)
 
 #
 # Test interpolation
 #
 # 1D interpolation points (for 2D RT problems along adjacent edges)
 nx = 3  # number of points
-xx = rand(Uniform(2,N-1),nx)
-fx = interp_cubic(one, fp, xx)
-println(fx)
+xa = rand(Uniform(2,N-1),nx)
+x = xa[1]
+#fx = bezier_interp_1dv(one, x)
+#println(fx)
 
 # 2D interpolation points (for 3D RT problems on adjacent faces)
 xx2 = rand(Uniform(2,N-1),nx,2)
-
+xx = xx2[:,1]
+yy = xx2[:,2]
+xi2 = floor.(Int, xx2)
+xi = xi2[:,1]
+yi = xi2[:,2]
